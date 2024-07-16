2694371e380614516ad46cc7c2a72a70dc03811f
Akos Komuves <akoskm@gmail.com>
2024-07-16 07:31:56 +0200

    remove unused import

--- fetchTodos.ts
+++ fetchTodos.ts
@@ -1,12 +1,10 @@
 import { TODO_KEY } from "@/constants";
-import { syncTodos } from "@/syncTodos";
 import { createClient } from "@/utils/supabase/client";
 
 export async function fetchTodos(setTodos) {
   const supabase = createClient();
 
   const localTodos = JSON.parse(localStorage.getItem(TODO_KEY) ?? "[]");
-  // await syncTodos();
 
   const { data: supabaseTodos } = await supabase
     .from("todos")
